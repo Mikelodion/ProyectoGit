1ba079f2f30f8773ce048f71340279c6a3303cdf
Miquel Janer <miqueljanermudoy@outlook.com>
2020-02-13 04:36:25 +0100

    V.2 RollDice

--- app.js
+++ app.js
@@ -5,5 +5,50 @@ GAME RULES:git
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-Edicion de prueba.
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.btn-roll').addEventListener('click',rollDice);
+document.querySelector('.btn-hold').addEventListener('click',holdRoundScore);
+function rollDice(){
+    //Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //Display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display  = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+
+    if(dice !== 1){
+        //Sum to current
+        roundScore += dice;
+        document.getElementById('current-'+activePlayer).innerHTML = roundScore;
+    }else{
+        //Switch Players
+        roundScore = 0;
+        changeUI();
+    }
+}
+function holdRoundScore(){
+    scores[activePlayer] += roundScore;
+    roundScore = 0;
+    document.querySelector('#score-'+activePlayer).innerHTML = scores[activePlayer]
+    if(scores[activePlayer] >= 20){
+        document.querySelector('#name-'+activePlayer).textContent = "WINNER!"
+        window.addEventListener('click', function (event) {
+            event.stopPropagation();
+        }, true);
+    }
+    changeUI();
+}
+function changeUI(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
